a9cc555359aa1343dc7dd465dac6611d8d585595
Eduardo Flores <eduardofv@gmail.com>
2023-03-27 16:46:00 -0600

    closure_point.rs: prueba de HashMap de closures

--- closure_point.rs
+++ closure_point.rs
@@ -2,14 +2,15 @@ use std::{cell::RefCell, rc::Rc};
 use std::collections::HashMap;
 
 //CREATING A HASHMAP WITH CLOSURES LOOKS PROBLEMATIC
-fn closure_point() -> HashMap<&'static str, &'static dyn FnOnce()> {
+fn closure_point() -> Box<dyn Fn()> {
+//fn closure_point() -> HashMap<&'static str, Box<&'static dyn Fn()>> {
     let x = Rc::new(RefCell::new(0));
 
-    let mut methods = HashMap::new();
+    //let mut methods = HashMap::new();
 
     let x_add = x.clone();
     let add = move || { *x_add.borrow_mut() += 1; };    
-    methods.insert("add", &add);
+    //methods.insert("add", Box::new(&add));
 
     /*
     let x_get = x.clone();
@@ -19,13 +20,18 @@ fn closure_point() -> HashMap<&'static str, &'static dyn FnOnce()> {
     }));
     */
 
-    return methods;
+    return Box::new(add);
+    //return methods;
 }
 
 
 fn main() {
-    let obj = closure_point();
+    let mut object = closure_point();
 
-    println!("{}", obj.get("add")());
+    let mut hm: HashMap<&str, &mut Box<dyn Fn()>> = HashMap::new();
+    hm.insert("add", &mut object);
+
+    object();
+//    println!("{}", object());
 }
 
